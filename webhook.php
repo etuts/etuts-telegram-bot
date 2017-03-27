c54a66ef2dff380f5cb02b34dc7ee3c4892ff69a
Vahid Mohammadi <email@vmoh.ir>
2017-03-27 17:05:18 +0430

    setup bot

--- webhook.php
+++ webhook.php
@@ -3,10 +3,13 @@ require 'vendor/autoload.php';
 use Telegram\Bot\Api;
 
 $telegram = new Api('291144367:AAF66QzZVlw8MH0c8RyCD9hmnYnzRRrqMWs');
-$response = $telegram->getMe();
 
-$botId = $response->getId();
-$firstName = $response->getFirstName();
-$username = $response->getUsername();
-echo $firstName;
+$updates = $telegram->getWebhookUpdates();
+
+$response = $telegram->sendMessage([
+  'chat_id' => '92454', 
+  'text' => var_export($updates);
+]);
+
+// $messageId = $response->getMessageId();
 ?>
