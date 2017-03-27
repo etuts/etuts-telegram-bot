87bd18a0a3cad2ef2f91e4865c62878be8b682bf
Vahid Mohammadi <email@vmoh.ir>
2017-03-28 02:37:36 +0430

    a lot of changes

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 // database functions 
 function db_get_user_row($chat_id) {
 	global $db;
-	mysqli_query($db, "SELECT * FROM `chats` WHERE chat_id = '$chat_id' ");
+	return mysqli_query($db, "SELECT * FROM `chats` WHERE chat_id = '$chat_id' ");
 }
 function db_get_state($chat_id) {
 	global $db;
@@ -12,15 +12,15 @@ function db_get_state($chat_id) {
 }
 function db_insert($chat_id, $state, $text) {
 	global $db;
-	mysqli_query($db, "INSERT INTO `chats` (chat_id, state, last_message) VALUES ('$chat_id', '$state', '$text') ");
+	return mysqli_query($db, "INSERT INTO `chats` (chat_id, state, last_message) VALUES ('$chat_id', '$state', '$text') ");
 }
 function db_update_last_message($chat_id, $text) {
 	global $db;
-	mysqli_query($db, "UPDATE `chats` SET last_message = '$text' WHERE chat_id = '$chat_id' ");
+	return mysqli_query($db, "UPDATE `chats` SET last_message = '$text' WHERE chat_id = '$chat_id' ");
 }
 function db_update_state($chat_id, $state) {
 	global $db;
-	mysqli_query($db, "UPDATE `chats` SET state = '$state' WHERE chat_id = '$chat_id' ");
+	return mysqli_query($db, "UPDATE `chats` SET state = '$state' WHERE chat_id = '$chat_id' ");
 }
 
 // telegram bot api functions
