b1ee0f912f2160d34ed615089b02d567997e9dd7
Vahid Mohammadi <email@vmoh.ir>
2017-03-27 19:54:45 +0430

    connecting to database

--- webhook.php
+++ webhook.php
@@ -15,12 +15,13 @@ $text = $updates->getMessage()->getText();
 $db = mysqli_connect('localhost','ZMYbZ5jIaqW5SYi','bzJcaSbjlgtp9K9','etutsTeleRobot') or die('Error connecting to MySQL server.');
 
 // check chat state
-// $result = mysql_query("SELECT * FROM chats WHERE chat_id ='$chat_id' ");
+$result = mysql_query("SELECT * FROM chats WHERE chat_id ='$chat_id' ");
 $state = "nothing";
-/*if( mysql_num_rows($result) < 0)
+if( mysql_num_rows($result) == 0)
     mysql_query("INSERT INTO chats (chat_id) VALUES ('$chat_id') ");
+	$state = "added";
 else
-	$state = mysql_query("SELECT state from chats where chat_id = $chat_id");*/
+	$state = mysql_query("SELECT state from chats where chat_id = $chat_id");
 
 $telegram->sendMessage([
   'chat_id' => $chat_id,
