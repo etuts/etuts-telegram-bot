ff46b926240402016b2e87121cf2ae9bb8ee09c3
Vahid Mohammadi <email@vmoh.ir>
2017-03-27 19:49:24 +0430

    connecting to database

--- webhook.php
+++ webhook.php
@@ -1,15 +1,33 @@
 <?php 
+// requirements
 require 'vendor/autoload.php';
 use Telegram\Bot\Api;
 
+// connecting
 $telegram = new Api('291144367:AAF66QzZVlw8MH0c8RyCD9hmnYnzRRrqMWs');
 
+// get user message
 $updates = $telegram->getWebhookUpdates();
+$chat_id = $updates->getMessage()->getChat()->getId();
+$text = $updates->getMessage()->getText();
 
-$response = $telegram->sendMessage([
-  'chat_id' => '92454', 
-  'text' => var_export($updates);
+// connect to database
+$db = mysqli_connect('localhost','ZMYbZ5jIaqW5SYi','bzJcaSbjlgtp9K9','etutsTeleRobot') or die('Error connecting to MySQL server.');
+
+// check chat state
+$result = mysql_query("SELECT * FROM chats WHERE chat_id ='$chat_id' ");
+$state = "nothing";
+if( mysql_num_rows($result) < 0)
+    mysql_query("INSERT INTO chats (chat_id) VALUES ('$chat_id') ");
+else
+	$state = mysql_query("SELECT state from chats where chat_id = $chat_id");
+
+$telegram->sendMessage([
+  'chat_id' => $chat_id,
+  'text' => $state
 ]);
 
-// $messageId = $response->getMessageId();
+// close database
+mysqli_close($db);
+
 ?>
