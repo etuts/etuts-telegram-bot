9dabf3210892c85846aff6e95c58614293c3193c
Vahid Mohammadi <email@vmoh.ir>
2017-03-27 16:46:21 +0430

    setup bot

---
+++ webhook.php
@@ -0,0 +1,11 @@
+<?php 
+use Telegram\Bot\Api;
+
+$telegram = new Api('291144367:AAF66QzZVlw8MH0c8RyCD9hmnYnzRRrqMWs');
+$response = $telegram->getMe();
+
+$botId = $response->getId();
+$firstName = $response->getFirstName();
+$username = $response->getUsername();
+echo $firstName;
+?>
