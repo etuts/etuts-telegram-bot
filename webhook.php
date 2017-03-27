465573d7cc719c9eda79359e9db80c33b602acfe
Vahid Mohammadi <email@vmoh.ir>
2017-03-27 20:26:23 +0430

    connecting to database

--- webhook.php
+++ webhook.php
@@ -8,22 +8,23 @@ $telegram = new Api('291144367:AAF66QzZVlw8MH0c8RyCD9hmnYnzRRrqMWs');
 
 // get user message
 $updates = $telegram->getWebhookUpdates();
-$chat_id = $updates->getMessage()->getChat()->getId();
+$chat_id = (int) $updates->getMessage()->getChat()->getId();
 $text = $updates->getMessage()->getText();
 
 // connect to database
 $db = mysqli_connect('localhost','ZMYbZ5jIaqW5SYi','bzJcaSbjlgtp9K9','etutsTeleRobot') or die('Error connecting to MySQL server.');
 
 // check chat state
-$result = mysql_query("SELECT * FROM chats WHERE chat_id ='$chat_id' ");
-$state = "nothing";
-$state = mysql_num_rows($result);
-/*if( mysql_num_rows($result) == 0) {
-    mysql_query("INSERT INTO chats (chat_id, state, last_message) VALUES ('$chat_id', '0', '$text') ");
+$result = mysqli_query($db, "SELECT * FROM `chats` WHERE chat_id = '$chat_id' ");
+$state = "ef";
+if( mysqli_num_rows($result) == 0) {
+    mysqli_query($db, "INSERT INTO `chats` (chat_id, state, last_message) VALUES ('$chat_id', '0', '$text') ");
 	$state = "added";
-}*/
-/*else
-	$state = mysql_query("SELECT state from chats where chat_id = $chat_id");*/
+}
+else {
+	$result = mysqli_query($db, "SELECT `state` FROM `chats` WHERE chat_id = '$chat_id' ");
+	$state = $result->fetch_assoc()['state'];
+}
 
 $telegram->sendMessage([
   'chat_id' => $chat_id,
