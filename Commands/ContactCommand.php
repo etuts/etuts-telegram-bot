126b0e7c5369d2dba3efd5dea3d68d0dc9fb88e0
Vahid Mohammadi <email@vmoh.ir>
2017-04-06 16:21:14 +0430

    installing commands

--- Commands/ContactCommand.php
+++ Commands/ContactCommand.php
@@ -1,54 +1,34 @@
 <?php
 
-namespace Vendor\App\Commands;
+namespace Telegram\Bot\Commands;
 
-use Telegram\Bot\Actions;
-use Telegram\Bot\Commands\Command;
-
+/**
+ * Class ContactCommand.
+ */
 class ContactCommand extends Command
 {
     /**
      * @var string Command Name
      */
-    protected $name = "contact";
+    protected $name = 'contact';
 
     /**
      * @var string Command Description
      */
-    protected $description = "contact command";
+    protected $description = 'contact command';
 
     /**
-     * @inheritdoc
+     * {@inheritdoc}
      */
     public function handle($arguments)
     {
-        // This will send a message using `sendMessage` method behind the scenes to
-        // the user/chat id who triggered this command.
-        // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
-        // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
-        $this->replyWithMessage(['text' => 'Contact Command test']);
+        $commands = $this->telegram->getCommands();
 
-        // This will update the chat status to typing...
-        $this->replyWithChatAction(['action' => Actions::TYPING]);
-
-        // This will prepare a list of available commands and send the user.
-        // First, Get an array of all registered commands
-        // They'll be in 'command-name' => 'Command Handler Class' format.
-        $commands = $this->getTelegram()->getCommands();
-
-        // Build the list
-        $response = '';
-        foreach ($commands as $name => $command) {
-            $response .= sprintf('/%s - %s' . PHP_EOL, $name, $command->getDescription());
+        $text = '';
+        foreach ($commands as $name => $handler) {
+            $text .= sprintf('/%s - %s'.PHP_EOL, $name, $handler->getDescription());
         }
 
-        // Reply with the commands list
-        $this->replyWithMessage(['text' => $response]);
-
-        // Trigger another command dynamically from within this command
-        // When you want to chain multiple commands within one or process the request further.
-        // The method supports second parameter arguments which you can optionally pass, By default
-        // it'll pass the same arguments that are received for this command originally.
-        $this->triggerCommand('subscribe');
+        $this->replyWithMessage(compact('text'));
     }
 }
