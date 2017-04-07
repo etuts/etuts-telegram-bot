973b1a8acaff2d399ca5a71cc4c71509f6ca9a33
Vahid Mohammadi <email@vmoh.ir>
2017-04-07 10:26:27 +0430

    adding commands

--- Commands/ContactCommand.php
+++ Commands/ContactCommand.php
@@ -1,26 +1,34 @@
 <?php
-require '../functions.php';
 
-// namespace CustomCommands;
-namespace Vendor\App\Commands;
+namespace Telegram\Bot\Commands;
 
-use Telegram\Bot\Actions;
-use Telegram\Bot\Commands\Command;
-
-class ContactCommand extends Command
+/**
+ * Class HelpCommand.
+ */
+class HelpCommand extends Command
 {
-	protected $name = 'contact';
-	protected $description = 'contact command';
+    /**
+     * @var string Command Name
+     */
+    protected $name = 'help';
 
-	public function handle($arguments)
-	{
-		$text = 'لطفا پیام تان را بفرستید.';
-		$this->replyWithMessage(compact('text'));
-		// test these ones
-		$this->getChat()->getId();
-		$this->getUpdate();
-		$this->getTelegram();
+    /**
+     * @var string Command Description
+     */
+    protected $description = 'Help command, Get a list of commands';
 
-		db_set_state($chat_id, CONTACT);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    public function handle($arguments)
+    {
+        $commands = $this->telegram->getCommands();
+
+        $text = '';
+        foreach ($commands as $name => $handler) {
+            $text .= sprintf('/%s - %s'.PHP_EOL, $name, $handler->getDescription());
+        }
+
+        $this->replyWithMessage(compact('text'));
+    }
 }
