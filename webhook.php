2c55bb35aede30d7221dc0fe3137939a7bf96856
Vahid Mohammadi <email@vmoh.ir>
2017-04-07 20:02:12 +0430

    adding commands

--- webhook.php
+++ webhook.php
@@ -1,15 +1,10 @@
 <?php
-
-// connect to database
-$db = mysqli_connect('localhost','ZMYbZ5jIaqW5SYi','bzJcaSbjlgtp9K9','etutsTeleRobot') or die('Error connecting to MySQL server.');
-
 // requirements
 require 'vendor/autoload.php';
-require 'functions.php';
 use Telegram\Bot\Api;
 
 // connecting
-$telegram = new Api('291144367:AAF66QzZVlw8MH0c8RyCD9hmnYnzRRrqMWs');
+$telegram = new Api('TOKEN');
 
 // get user message
 $updates = $telegram->getWebhookUpdates();
@@ -20,41 +15,9 @@ $telegram->addCommand(Telegram\Bot\Commands\HelpCommand::class);
 $telegram->addCommand(CustomCommands\ContactCommand::class);
 $update = $telegram->commandsHandler(true);
 
-// Enum of STATEs
-define("CANCEL", 0);
-define("CONTACT", 1);
-
-// get chat state from database
-$result = db_get_user_row($chat_id);
-$state = CANCEL; // no state
-if( mysqli_num_rows($result) == 0) {
-    db_insert($chat_id, 0, $text);
-}
-else {
-	$state = db_get_state($chat_id);
-	db_update_last_message($chat_id, $text);
-}
-
-// switch case
-switch ($state) {
-	case CANCEL:
-		// user has sent chert o pert! execute help command
-		break;
-	case CONTACT:
-		// user has sent a message to admin! Wow!!
-		send_message_to_admin($text);
-		db_reset_state($chat_id);
-		break;
-	
-	default:
-		# code...
-		break;
-}
 $telegram->sendMessage([
   'chat_id' => $chat_id,
-  'text' => $state
+  'text' => $text
 ]);
 
-// close database
-mysqli_close($db);
 ?>
