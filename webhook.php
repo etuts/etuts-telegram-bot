59adfec06c5693f9f353fc10832b48b5f8fac673
Vahid Mohammadi <email@vmoh.ir>
2017-04-10 20:12:22 +0430

    reset webhoook

--- webhook.php
+++ webhook.php
@@ -1,6 +1,8 @@
 <?php
+
 // requirements
 require 'vendor/autoload.php';
+require 'functions.php';
 use Telegram\Bot\Api;
 
 // connecting
@@ -10,14 +12,44 @@ $telegram = new Api('291144367:AAF66QzZVlw8MH0c8RyCD9hmnYnzRRrqMWs');
 $updates = $telegram->getWebhookUpdates();
 $chat_id = (int) $updates->getMessage()->getChat()->getId();
 $text = $updates->getMessage()->getText();
-
 $telegram->addCommand(Telegram\Bot\Commands\HelpCommand::class);
 $telegram->addCommand(Telegram\Bot\Commands\ContactCommand::class);
 $update = $telegram->commandsHandler(true);
 
-$telegram->sendMessage([
-  'chat_id' => $chat_id,
-  'text' => $text
-]);
 
+// Enum of STATEs
+define("CANCEL", 0);
+define("CONTACT", 1);
+
+// get chat state from database
+$result = db_get_user_row($chat_id);
+$state = CANCEL; // no state
+if( mysqli_num_rows($result) == 0) {
+    db_insert($chat_id, 0, $text);
+}
+else {
+	$state = db_get_state($chat_id);
+	db_update_last_message($chat_id, $text);
+}
+
+// switch case
+switch ($state) {
+	case CANCEL:
+		// user has sent chert o pert! execute help command
+		break;
+	case CONTACT:
+		// user has sent a message to admin! Wow!!
+		send_message_to_admin($text);
+		db_reset_state($chat_id);
+		break;
+	
+	default:
+		# code...
+		break;
+}
+
+// $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $state ]);
+
+// close database
+mysqli_close($db);
 ?>
