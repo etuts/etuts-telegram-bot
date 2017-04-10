c4ed9135ede63d345a0d3055e42b96c05a6deb2e
Vahid Mohammadi <email@vmoh.ir>
2017-04-11 02:02:56 +0430

    lots of changes

--- functions.php
+++ functions.php
@@ -1,6 +1,11 @@
 <?php
 
-// database functions 
+//--------------------- Enum of STATEs ----------------------
+define("CANCEL", 0);
+define("CONTACT", 1);
+
+
+//--------------------- database functions ------------------
 function db_get_user_row($chat_id) {
 	global $db;
 	return mysqli_query($db, "SELECT * FROM `chats` WHERE chat_id = '$chat_id' ");
@@ -26,13 +31,57 @@ function db_reset_state($chat_id) {
 	return db_set_state($chat_id,0);
 }
 
-// telegram bot api functions
+// get chat state from database
+function get_chat_state($chat_id) {
+	$result = db_get_user_row($chat_id);
+	$state = CANCEL; // no state
+	if( mysqli_num_rows($result) == 0) {
+	    db_insert($chat_id, 0, $text);
+	}
+	else {
+		$state = db_get_state($chat_id);
+		db_update_last_message($chat_id, $text);
+	}
+	return $state;
+}
+
+
+//--------------------- telegram bot api functions ---------------
 function send_message_to_admin($text) {
 	global $telegram;
 	$telegram->sendMessage([
 	  'chat_id' => 92454,
 	  'text' => $text
 	]);
+}
+function get_command($text) {
+	$available_commands = ['/contact'];
+	$contain_these_commands = array();
+	foreach ($available_commands as $command) {
+		if (contains_word($text, $command))
+			$contain_these_commands[] = $command;
+	}
+	return $contain_these_commands;
+}
+function run_command($command, $chat_id, $text, $message_id) {
+	global $telegram;
+	switch ($command) {
+		case '/contact':
+			$telegram->sendMessage([
+				'chat_id' => $chat_id,
+				'text' => 'لطفا پیام بفرستید',
+				'reply_to_message_id' => $message_id
+			])
+			db_set_state($chat_id, CONTACT)
+			break;
+	}
+}
+
+//--------------------- helpers -------------------------------
+function contains_word($source, $find) {
+	if (strpos($text, '/contact') !== false)
+		return true;
+	return false;	
 }
 
 ?>
