731c6cd434f2d9e17f1874e00af460cbfc1c4bc1
Vahid Mohammadi <email@vmoh.ir>
2017-04-12 20:14:47 +0430

    debug

--- functions.php
+++ functions.php
@@ -155,7 +155,7 @@ function run_scheduale_post_command($chat_id, $text, $message_id, $message) {
 			'resize_keyboard' => true, 
 			'one_time_keyboard' => true
 		]);
-		$reply_markup = $telegram->forceReply();
+		// $reply_markup = $telegram->forceReply();
 	} else {
 		$answer = 'برای استفاده از این دستور باید ادمین کانال باشید';
 	}
