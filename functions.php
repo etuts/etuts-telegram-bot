3bbbc392257f96a7584a288138fbafee4e392ee8
Vahid Mohammadi <email@vmoh.ir>
2017-04-12 21:38:31 +0430

    debug

--- functions.php
+++ functions.php
@@ -105,8 +105,7 @@ function get_keyboard_button($text) {
 	return $keyboard_button;
 }
 function run_keyboard_button_func($btn,$text,$chat_id, $message_id, $message) {
-	// setlocale(LC_CTYPE, 'nl_BE.utf8');
-	log_debug(iconv("UTF-8", "ISO-8859-1//TRANSLIT", $text));
+	log_debug(convert_to_english($text));
 	// global $keyboard_buttons;
 	// switch ($btn) {
 	// 	case $:
@@ -270,6 +269,11 @@ function log_debug($text) {
 	$debug_file = fopen("log.txt","a");
 	fwrite($debug_file, $text . PHP_EOL . "-------------------------\r\n");
 	fclose($file);
+}
+function convert_to_english($text) {
+	$persian = array('ض','ص','ث','ق','ف','غ','ع','ه','خ','ح','ج','چ','ش','س','ی','ب','ل','ا','ت','ن','م','ک','گ','ظ','ط','ز','ر','ذ','د','پ','و',' ');
+	$english = array('q','w','e','r','t','y','u','i','o','p','_1','_2','a','s','d','f','g','h','j','k','l','_3','_4','z','x','cv','b','n','m','_5','_');
+	return str_replace($persian, $english, $text);
 }
 
 ?>
