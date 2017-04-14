8745a5529b66ada20ea621d3e366720855151c86
Vahid Mohammadi <email@vmoh.ir>
2017-04-15 01:52:34 +0430

    debug

--- functions.php
+++ functions.php
@@ -149,7 +149,7 @@ function run_help_command($chat_id, $text, $message_id, $message) {
 	global $telegram, $available_commands;
 	$answer = '';
 	foreach ($available_commands as $index => $command) {
-		$answer .= sprintf('%s - %s'.PHP_EOL, $index, $command);
+		$answer .= sprintf('%s'.PHP_EOL, $command);
 	}
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
