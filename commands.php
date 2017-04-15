8ab19832bd4e2b081e589eefa694f45ae5d1bba1
MohGanji <mohammad-ganji@outlook.com>
2017-04-15 23:44:58 +0430

    Changed commands formats, and added description section for test

--- commands.php
+++ commands.php
@@ -1,8 +1,25 @@
 <?php 
-$available_commands = ['/contact','/post_validation','/cancel','/schedule_post','/start','/help'];
+
+$available_commands = [
+
+	"/contact" => array("name"=>"/contact", "description"=>"description of contact"),
+
+	"/post_validation" => array("name"=>"/post_validation", "description"=>"description of post_validation"),
+	
+	"/cancel" => array("name"=>"/cancel", "description"=>"description of cancel"),
+
+	"/schedule_post" => array("name"=>"/schedule_post", "description"=>"description of schedule_post"),
+
+	"/start" => array("name"=>"/start", "description"=>"description of start"),
+
+	"/help" => array("name"=>"/help", "description"=>"description of help")
+
+];
 
 foreach (glob("./commands/*.php") as $filename) {
     require ($filename);
 }
 
 ?>
+
+
