2e60b92ec4545391ba581009a15a584770d98bc7
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 01:59:01 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -9,7 +9,7 @@ function run_help_command($chat_id, $text, $message_id, $message) {
 	// $answer .= $admins;
 	foreach ($available_commands as $command) {
 		// $answer .= sprintf('%s'.PHP_EOL, $command["name"]);
-		$answer .= ("/".$command["name"]."\n");
+		$answer .= ("/".$command["name"]." - ".$command["description"]."\n");
 	}
 	if ($is_admin)
 		$answer .= "Admin";
