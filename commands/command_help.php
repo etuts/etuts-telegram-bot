f77ee2a76baa8bcdfee0fe2e0712b5ff68261ad8
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 00:52:36 +0430

    Help command improvement

--- commands/command_help.php
+++ commands/command_help.php
@@ -7,7 +7,6 @@ function run_help_command($chat_id, $text, $message_id, $message) {
 		// $answer .= sprintf('%s'.PHP_EOL, $command["name"]);
 		$answer .= ("/".$command["name"]."\n");
 	}
-	$answer .= "HIII";
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
 		'text' => $answer,
