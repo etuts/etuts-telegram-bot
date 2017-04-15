4d10f5fa7a9c5bc49a05f881c85fff0c1a6c554a
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 01:39:25 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -2,8 +2,8 @@
 
 function run_help_command($chat_id, $text, $message_id, $message) {
 	global $telegram, $available_commands,$db;
-	// $is_admin = check_user_permission(ADMIN);
-	$is_admin = true;
+	$is_admin = check_user_permission(ADMIN);
+	// $is_admin = true;
 	$answer = '';
 	// $answer .= $admins;
 	foreach ($available_commands as $command) {
