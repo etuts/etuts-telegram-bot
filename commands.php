a3ff3053229bf401cc29a1003bcf02a25f8304f7
MohGanji <mohammad-ganji@outlook.com>
2017-04-16 00:24:13 +0430

    fixed bugs related to changing commands formats.

--- commands.php
+++ commands.php
@@ -2,17 +2,17 @@
 
 $available_commands = [
 
-	"/contact" => array("name"=>"/contact", "description"=>"description of contact"),
+	"/contact" => array("name"=>"contact", "description"=>"description of contact"),
 
-	"/post_validation" => array("name"=>"/post_validation", "description"=>"description of post_validation"),
+	"/post_validation" => array("name"=>"post_validation", "description"=>"description of post_validation"),
 	
-	"/cancel" => array("name"=>"/cancel", "description"=>"description of cancel"),
+	"/cancel" => array("name"=>"cancel", "description"=>"description of cancel"),
 
-	"/schedule_post" => array("name"=>"/schedule_post", "description"=>"description of schedule_post"),
+	"/schedule_post" => array("name"=>"schedule_post", "description"=>"description of schedule_post"),
 
-	"/start" => array("name"=>"/start", "description"=>"description of start"),
+	"/start" => array("name"=>"start", "description"=>"description of start"),
 
-	"/help" => array("name"=>"/help", "description"=>"description of help")
+	"/help" => array("name"=>"help", "description"=>"description of help")
 
 ];
 
