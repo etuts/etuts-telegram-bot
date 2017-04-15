7410fbf0dae9a208f10d6ce93a479b3b953905e0
Vahid Mohammadi <email@vmoh.ir>
2017-04-16 01:00:42 +0430

    add permission to commands

--- commands.php
+++ commands.php
@@ -2,12 +2,12 @@
 
 $available_commands = [
 
-	"/contact" => array("name"=>"contact", "description"=>"description of contact"),
-	"/post_validation" => array("name"=>"post_validation", "description"=>"description of post_validation"),
-	"/cancel" => array("name"=>"cancel", "description"=>"description of cancel"),
-	"/schedule_post" => array("name"=>"schedule_post", "description"=>"description of schedule_post"),
-	"/start" => array("name"=>"start", "description"=>"description of start"),
-	"/help" => array("name"=>"help", "description"=>"description of help"),
+	"/contact" => array("name"=>"contact", "description"=>"description of contact", "permission"=>USER),
+	"/post_validation" => array("name"=>"post_validation", "description"=>"description of post_validation", "permission"=>AUTHOR),
+	"/cancel" => array("name"=>"cancel", "description"=>"description of cancel", "permission"=>USER),
+	"/schedule_post" => array("name"=>"schedule_post", "description"=>"description of schedule_post", "permission"=>ADMIN),
+	"/start" => array("name"=>"start", "description"=>"description of start", "permission"=>USER),
+	"/help" => array("name"=>"help", "description"=>"description of help", "permission"=>USER),
 
 ];
 
