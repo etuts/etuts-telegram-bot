49eabd2c9c38dd88a2619bc76021a7de0c565606
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 01:55:28 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -11,7 +11,10 @@ function run_help_command($chat_id, $text, $message_id, $message) {
 		// $answer .= sprintf('%s'.PHP_EOL, $command["name"]);
 		$answer .= ("/".$command["name"]."\n");
 	}
-	// $answer .= $permission;
+	if ($is_admin)
+		$answer .= "Admin";
+	else
+		$answer .= "Not Admin";
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
 		'text' => $answer,
