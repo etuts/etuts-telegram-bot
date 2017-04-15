cf5cc69b563da7d2a69a805771fa89d7a9ab6703
Vahid Mohammadi <email@vmoh.ir>
2017-04-15 17:36:43 +0430

    debug

--- functions.php
+++ functions.php
@@ -2,8 +2,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// require_once 'config.php';
-
 //--------------------- Enum of permissions ----------------------
 define("ADMIN", 1);
 define("AUTHOR", 2);
