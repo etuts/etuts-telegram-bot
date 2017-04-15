07a0e0fc89c748caa44520e730efde94c30a510e
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 01:34:40 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -2,13 +2,17 @@
 
 function run_help_command($chat_id, $text, $message_id, $message) {
 	global $telegram, $available_commands,$db;
-	$admins = $db->get_admins();
+	$is_admin = check_user_permission(ADMIN);
 	$answer = '';
 	// $answer .= $admins;
 	foreach ($available_commands as $command) {
 		// $answer .= sprintf('%s'.PHP_EOL, $command["name"]);
 		$answer .= ("/".$command["name"]."\n");
 	}
+	if ($is_admin)
+			$answer.("You are Admin\n");
+	else
+		$answer.("You are note Admin\n");
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
 		'text' => $answer,
