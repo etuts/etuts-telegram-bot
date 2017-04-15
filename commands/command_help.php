c59c4e743640d8c61f0ccf9d52fa56df98ee57f8
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 01:47:51 +0430

    Revert

--- commands/command_help.php
+++ commands/command_help.php
@@ -4,14 +4,14 @@ function run_help_command($chat_id, $text, $message_id, $message) {
 	global $telegram, $available_commands,$db;
 	// $is_admin = check_user_permission(ADMIN);
 	// $is_admin = true;
-	$permission = get_user_permission();
+	// $permission = get_user_permission();
 	$answer = '';
 	// $answer .= $admins;
 	foreach ($available_commands as $command) {
 		// $answer .= sprintf('%s'.PHP_EOL, $command["name"]);
 		$answer .= ("/".$command["name"]."\n");
 	}
-	$answer .= $permission;
+	// $answer .= $permission;
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
 		'text' => $answer,
