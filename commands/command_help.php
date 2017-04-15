4b01e74957737fd53243a7d08b3b1fb253930d37
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 01:18:36 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -4,7 +4,7 @@ function run_help_command($chat_id, $text, $message_id, $message) {
 	global $telegram, $available_commands,$db;
 	$admins = $db->get_admins();
 	$answer = '';
-	$answer .= $admins;
+	// $answer .= $admins;
 	foreach ($available_commands as $command) {
 		// $answer .= sprintf('%s'.PHP_EOL, $command["name"]);
 		$answer .= ("/".$command["name"]."\n");
