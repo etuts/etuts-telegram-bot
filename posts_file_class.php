3db7e101c1d2e174300927159a6451221cda9edf
Vahid Mohammadi <email@vmoh.ir>
2017-04-15 21:03:48 +0430

    add post and read post methods

--- posts_file_class.php
+++ posts_file_class.php
@@ -1,32 +1,44 @@
 <?php 
+define("LAST", -1);
 class Posts_file {
 	protected $pfile_name;
-	protected $has_read_file;
-	protected $has_write_file;
-	protected $read_file;
-	protected $write_file;
-	function __construct($pfile_name) {
+
+	protected $the_file; // FILE type
+
+	function __construct($pfile_name, $for_write = true) {
 		$this->pfile_name = $pfile_name;
+		if ($for_write)
+			$this->open_write_file();
+		else
+			$this->open_read_file();
+	}
+	function __destruct() {
+		fclose($this->the_file);
 	}
 	function open_read_file() {
-		$this->read_file = fopen($this->pfile_name, "r");
-		$this->has_read_file = true;
-		return $this->read_file;
+		$this->the_file = fopen($this->pfile_name, "r");
+		return $this->the_file;
 	}
 	function open_write_file() {
-		$this->write_file = fopen($this->pfile_name, "a");
-		$this->has_write_file = true;
-		return $this->write_file;
+		$this->the_file = fopen($this->pfile_name, "a");
+		return $this->the_file;
 	}
-	function close_files() {
-		if ($this->has_read_file)
-			fclose($this->read_file);
-
-		if ($this->has_write_file)
-			fclose($this->write_file);
+	function add_post($post_line, $priority = LAST) {
+		if (is_writable($this->the_file)) {
+			fwrite($this->the_file, $post_line);
+		}
 	}
-	function add_post($post_line) {
-		$this->open_write_file();
+	function read_post() {
+		$post_line = '';
+		if (is_readable($this->the_file)) {
+			if (!feof($this->the_file)) {
+				$post_line = fgets($this->the_file);
+				$contents = file_get_contents($this->pfile_name);
+				$contents = str_replace($post_line, '', $contents);
+				file_put_contents($this->pfile_name, $contents);
+			}
+		}
+		return $post_line;
 	}
 }
 ?>
