f2adc3f2770b99ab545b200a4e63a4cafafe2232
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 00:37:26 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -3,8 +3,8 @@
 function run_help_command($chat_id, $text, $message_id, $message) {
 	global $telegram, $available_commands;
 	$answer = '';
-	foreach ($available_commands as $index => $command) {
-		$answer .= sprintf('%s'.PHP_EOL, $command);
+	foreach ($available_commands as $command) {
+		$answer .= sprintf('%s'.PHP_EOL, $command->name);
 	}
 	$answer .= "HIII";
 	$telegram->sendMessage([
