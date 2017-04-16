18ca020249ba82732497b017514a8286cdd1cb25
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 15:41:05 +0430

    Test

--- commands/command_start.php
+++ commands/command_start.php
@@ -6,6 +6,12 @@ function run_start_command($chat_id, $text, $message_id, $message) {
 		'chat_id' => $chat_id,
 		'text' => 'خوش آمدید'
 	]);
+	$keyboard = [['معرفی ربات', 'معرفی ابزار']];
+		$reply_markup = $telegram->replyKeyboardMarkup([
+			'keyboard' => $keyboard, 
+			'resize_keyboard' => true, 
+			'one_time_keyboard' => true
+		]);
 
 	run_help_command($chat_id, $text, $message_id, $message);
 }
