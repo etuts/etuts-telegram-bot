ad8a62cfbf6d97e3d63f83b6e5e2f1ad82361e6d
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 14:14:49 +0430

    Test

--- commands/command_start.php
+++ commands/command_start.php
@@ -6,7 +6,6 @@ function run_start_command($chat_id, $text, $message_id, $message) {
 		'chat_id' => $chat_id,
 		'text' => 'خوش آمدید'
 	]);
-	run_keyboard_buttons($text, $chat_id, $message_id, $message);
 
 	run_help_command($chat_id, $text, $message_id, $message);
 }
