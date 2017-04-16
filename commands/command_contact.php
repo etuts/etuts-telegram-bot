e3d91f6d187f2657934c6d55d2ff208f15446ad6
MohGanji <mohammad-ganji@outlook.com>
2017-04-16 13:33:41 +0430

    fixed contact command reply bug

--- commands/command_contact.php
+++ commands/command_contact.php
@@ -5,7 +5,7 @@ function run_contact_command($chat_id, $text, $message_id, $message) {
 	global $telegram, $db;
 	$db->set_state(CONTACT);
 	$reply_markup = $telegram->forceReply();
-	reply($chat_id, 'لطفا پیام تان را بفرستید', true);
+	reply($chat_id, 'لطفا پیام تان را بفرستید', $message_id, true);
 }
 
 ?>
