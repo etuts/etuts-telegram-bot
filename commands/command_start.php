30c6ceb8561095046dfd5d20008f0b8eb99efe4f
sh-soltanpour <shahryar2006@gmail.com>
2017-04-16 15:51:45 +0430

    Test

--- commands/command_start.php
+++ commands/command_start.php
@@ -6,22 +6,22 @@ function run_start_command($chat_id, $text, $message_id, $message) {
 		'chat_id' => $chat_id,
 		'text' => 'خوش آمدید'
 	]);
-	$is_admin =  $db->check_user_permission(ADMIN);
-	$is_author = $db->check_user_permission(AUTHOR);
-	$permission = $is_admin ? ADMIN : $is_author ? AUTHOR : USER;
-	$commands = array();
-	foreach ($available_commands as $command) {
-		// $answer .= sprintf('%s'.PHP_EOL, $command["name"]);
-		if ($command["permission"] <= $permission)
-			// $answer .= ("/".$command["name"]." - ".$command["description"]."\n");
-			array_push($commands, $command["name"]);
-	}
-	$keyboard = [$commands];
-		$reply_markup = $telegram->replyKeyboardMarkup([
-			'keyboard' => $keyboard, 
-			'resize_keyboard' => true, 
-			'one_time_keyboard' => true
-		]);
+	// $is_admin =  $db->check_user_permission(ADMIN);
+	// $is_author = $db->check_user_permission(AUTHOR);
+	// $permission = $is_admin ? ADMIN : $is_author ? AUTHOR : USER;
+	// $commands = array();
+	// foreach ($available_commands as $command) {
+	// 	// $answer .= sprintf('%s'.PHP_EOL, $command["name"]);
+	// 	if ($command["permission"] <= $permission)
+	// 		// $answer .= ("/".$command["name"]." - ".$command["description"]."\n");
+	// 		array_push($commands, $command["name"]);
+	// }
+	// $keyboard = [$commands];
+	// 	$reply_markup = $telegram->replyKeyboardMarkup([
+	// 		'keyboard' => $keyboard, 
+	// 		'resize_keyboard' => true, 
+	// 		'one_time_keyboard' => true
+	// 	]);
 
 	run_help_command($chat_id, $text, $message_id, $message);
 }
