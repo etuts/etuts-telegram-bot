559a37180f478140def2f84b41e628c1bc94fe15
Vahid Mohammadi <email@vmoh.ir>
2017-04-19 23:08:11 +0430

    debug

--- commands/command_contact.php
+++ commands/command_contact.php
@@ -1,11 +1,13 @@
 <?php
 
-
 function run_contact_command($chat_id, $text, $message_id, $message) {
 	global $telegram, $db;
 	$db->set_state(CONTACT);
 	$reply_markup = $telegram->forceReply();
-	reply('لطفا پیام تان را بفرستید', $message_id, true);
+	$telegram->sendMessage([
+		'chat_id' => $chat_id,
+		'text' => 'لطفا پیام تان را بفرستید',
+		'reply_to_message_id' => $message_id,
+		'reply_markup' => $reply_markup
+	]);
 }
-
-?>
