2667fd78770cdcce93c168a32a80efbdf0ba0b08
Vahid Mohammadi <email@vmoh.ir>
2017-04-22 02:28:51 +0430

    add reply button to Contact_admin_answer

--- commands/command_contact.php
+++ commands/command_contact.php
@@ -20,11 +20,16 @@ function run_contact_command($chat_id, $text, $message_id, $message, $state) {
 			$dest_chat_id = $data['chat_id'];
 			$dest_message_id = $data['message_id'];
 			$fullname = get_fullname($message);
+
+			$keyboard = [ [['text' => 'پاسخ', 'callback_data' => '{"f":"admn_answr_cntct","c":'.(string)$chat_id.',"m":'.(string)$message_id.'}']] ];
+		    $reply_markup = Telegram\Bot\Keyboard\Keyboard::make([ 'inline_keyboard' => $keyboard, ]);
+
 			$telegram->sendMessage([
 				'chat_id' => $dest_chat_id,
 				'text' => '*پاسخ مدیر به پیام شما از طرف:* ' . $fullname . PHP_EOL . PHP_EOL . $text,
 				'parse_mode' => 'Markdown',
 				'reply_to_message_id' => $dest_message_id,
+				'reply_markup' => $reply_markup,
 			]);
 			$db->reset_state();
 			break;
