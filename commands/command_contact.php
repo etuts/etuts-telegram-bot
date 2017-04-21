4373d4f44300ae9ed5ad606ebc29ff69bcfaa69f
Vahid Mohammadi <email@vmoh.ir>
2017-04-22 01:14:52 +0430

    debug

--- commands/command_contact.php
+++ commands/command_contact.php
@@ -6,8 +6,10 @@ function run_contact_command($chat_id, $text, $message_id, $message, $state) {
 		case CONTACT:
 			// user has sent a message to admin! Wow!!
 			send_thank_message($message_id);
-			$keyboard = [ [['text' => 'پاسخ', 'callback_data' => '{"func":admin_answer_to_contact,"chat_id":'.$chat_id.',"message_id":'.$message_id.'}']] ];
-			$reply_markup = Telegram\Bot\Keyboard\Keyboard::make([ 'inline_keyboard' => $keyboard, ]);
+			$btn1 = ['text' => '7', 'url' => 'http://etuts.ir'];
+    $btn2 = ['text' => '7', 'url' => 'http://etuts.ir'];
+    $keyboard = [ [$btn1, $btn2] ];
+    $reply_markup = Telegram\Bot\Keyboard\Keyboard::make([ 'inline_keyboard' => $keyboard, ]);
 
 			log_debug("ok");
 
