309d70dc0fcd3aba4a09be1ba399a0793f828913
sh-soltanpour <shahryar2006@gmail.com>
2017-04-22 21:33:13 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -10,8 +10,11 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 		if ($command["permission"] <= $permission)
 			$answer .= ("/".$command["name"]." - ".$command["description"]."\n");
 	}
+	$emoticons = "\ud83d\udc4e";
+	// $data['text'] =  "your text ".json_decode('"'.$emoticons.'"').' bla bla';
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
-		'text' => $answer,
+		'text' => $answer.json_decode('"'.$emoticons.'"').' bla bla',
 	]);
+	
 }
