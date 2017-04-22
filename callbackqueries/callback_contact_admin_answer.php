e4691a24cc7600af4349e72f15221fd42a901467
Vahid Mohammadi <email@vmoh.ir>
2017-04-22 20:56:00 +0430

    debug

--- callbackqueries/callback_contact_admin_answer.php
+++ callbackqueries/callback_contact_admin_answer.php
@@ -4,12 +4,12 @@ function callback_admn_answr_cntct($id, $from, $message, $data) {
 	global $db;
 	$chat_id = $data['c'];
 	$message_id = $data['m'];
-	$username = get_username($chat_id);
+	$fullname = get_fullname($chat_id);
 
 	$db->set_state(CONTACT_ADMIN_ANSWER);
 	$db->set_data('{"chat_id":' . $chat_id . ',"message_id":' . $message_id . '}');
 
-	$answer_data = ['text' => 'در حال پاسخ به ' . $username];
+	$answer_data = ['text' => 'در حال پاسخ به ' . $fullname];
 	sendMessage($answer_data['text'], true);
 
 	return $answer_data;
