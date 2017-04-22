28b751721476611f59472c5c6ed2d64865965961
Vahid Mohammadi <email@vmoh.ir>
2017-04-22 18:49:28 +0430

    debug

--- keyboard_btns/btn_moarefi_robot.php
+++ keyboard_btns/btn_moarefi_robot.php
@@ -4,10 +4,13 @@ function btn_moarefi_robot($chat_id, $text, $message_id, $message, $state) {
 	global $db;
 	switch ($state) {
 		case MOAREFI_ROBOT:
-			$file = new Posts_file();
 			$post_line = $text;
 			log_debug("vahid");
-			$file->add_post($post_line);
+			
+			/*$file = new Posts_file();
+			$file->add_post($post_line);*/
+			$file = fopen("channel-posts.txt", "w");
+			fwrite($file, $post_line . PHP_EOL);
 			$db->reset_state();
 			break;
 		
