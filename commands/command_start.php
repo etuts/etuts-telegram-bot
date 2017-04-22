884300b94a5b9dc6b6ceb0f4ce3785452aa3f7f9
MohGanji <mohammad-ganji@outlook.com>
2017-04-22 19:41:21 +0430

    fixed bug in ignoring some commands

--- commands/command_start.php
+++ commands/command_start.php
@@ -7,14 +7,12 @@ function run_start_command($chat_id, $text, $message_id, $message, $state) {
 	$is_author = $db->check_user_permission(AUTHOR);
 	$permission = $is_admin ? ADMIN : $is_author ? AUTHOR : USER;
 	$commands = array();
-	$commands_to_ignore = ["help", "cancel", "start"];
+	$commands_to_ignore = array("help", "cancel", "start");
 	foreach ($available_commands as $command) {
 		// log_debug(var_export($command, true), 117990761);
-		// $answer .= sprintf('%s'.PHP_EOL, $command["name"]);
 		if ($command["permission"] <= $permission)
-			if(!in_array(command["name"], $commands_to_ignore))
-			// $answer .= ("/".$command["name"]." - ".$command["description"]."\n");
-			array_push($commands, $command["name"]);
+			if(!in_array($command["name"], $commands_to_ignore))
+				array_push($commands, $command["name"]);
 	}
 	$keyboard = oojoor($commands); // amin goft esmesho in bezar =)) baadan yeki avaz kone be couple
 									// araye intory bar migardoone:   [["0", "1"]
