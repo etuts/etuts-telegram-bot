9bbd0bb4de127951ebb1cd7bb42c582940bc9c8c
sh-soltanpour <shahryar2006@gmail.com>
2017-04-22 21:36:01 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -14,7 +14,7 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 	// $data['text'] =  "your text ".json_decode('"'.$emoticons.'"').' bla bla';
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
-		'text' => $answer.json_decode('"'.$emoticons.'"').' bla bla',
+		'text' => $answer.json_decode('"'.$emoticons.'"').' bla bla🤣',
 	]);
 	
 }
