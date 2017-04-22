7c7e3f98476a8096c77c0423b71f78b94d49084b
Vahid Mohammadi <email@vmoh.ir>
2017-04-22 18:41:48 +0430

    debug

--- keyboard_btns/btn_moarefi_robot.php
+++ keyboard_btns/btn_moarefi_robot.php
@@ -6,6 +6,7 @@ function btn_moarefi_robot($chat_id, $text, $message_id, $message, $state) {
 		case MOAREFI_ROBOT:
 			$file = new Posts_file();
 			$post_line = $text;
+			log_debug("vahid");
 			$file->add_post($post_line);
 			// $db->reset_state();
 			break;
