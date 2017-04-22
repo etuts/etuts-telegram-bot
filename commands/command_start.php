4db3f3d6ca339a8b326c17dab717cb95e1d8046c
MohGanji <mohammad-ganji@outlook.com>
2017-04-22 19:21:35 +0430

    Fixed commands in start keyboard

--- commands/command_start.php
+++ commands/command_start.php
@@ -7,15 +7,18 @@ function run_start_command($chat_id, $text, $message_id, $message, $state) {
 	$is_author = $db->check_user_permission(AUTHOR);
 	$permission = $is_admin ? ADMIN : $is_author ? AUTHOR : USER;
 	$commands = array();
+	$commands_to_ignore = ["help", "cancel", "start"];
 	foreach ($available_commands as $command) {
 		// log_debug(var_export($command, true), 117990761);
 		// $answer .= sprintf('%s'.PHP_EOL, $command["name"]);
 		if ($command["permission"] <= $permission)
-			if($command["name"] != "help")
+			if(!in_array(command["name"], $commands_to_ignore))
 			// $answer .= ("/".$command["name"]." - ".$command["description"]."\n");
 			array_push($commands, $command["name"]);
 	}
-	$keyboard = [$commands];
+	$keyboard = oojoor($commands); // amin goft esmesho in bezar =)) baadan yeki avaz kone be couple
+									// araye intory bar migardoone:   [["0", "1"]
+									//  								["2", "3"]] ...
 	$reply_markup = $telegram->replyKeyboardMarkup([
 		'keyboard' => $keyboard, 
 		'resize_keyboard' => true, 
@@ -32,3 +35,17 @@ function run_start_command($chat_id, $text, $message_id, $message, $state) {
 	// log_debug(var_export($state), 117990761);
 	//run_help_command($chat_id, $text, $message_id, $message, $state);
 }
+
+function oojoor($arr){
+	$ans = array();
+	$cnt = 0;
+	for($i = 0; $i < sizeof($arr)/2 ; $i += $cnt%0){
+		array_push($ans, []);
+		foreach($arr as $ind){
+			array_push($ans[i], $ind);
+		}
+	}
+	log_debug(var_export($ans, true), 117990761);
+}
+
+?>
