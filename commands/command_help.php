6e00522372dab2bd9d6420f407a49a2516eaa131
sh-soltanpour <shahryar2006@gmail.com>
2017-04-23 02:23:27 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -10,9 +10,6 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 		if ($command["permission"] <= $permission)
 			$answer .= ("/".$command["name"]." - ".$command["description"]."\n");
 	}
-	// $data['text'] =  "your text ".json_decode('"'.$emoticons.'"').' bla bla';
-	$post = get_last_post();
-	$answer .= $post->description;
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
 		'text' => $answer,
