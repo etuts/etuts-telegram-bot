bb9db0838767822c6c2ecf48908081436741abf3
MohGanji <mohammad-ganji@outlook.com>
2017-04-22 18:20:53 +0430

    debugging keyboard at start

--- commands/command_start.php
+++ commands/command_start.php
@@ -1,7 +1,7 @@
 <?php
 
 function run_start_command($chat_id, $text, $message_id, $message, $state) {
-	global $telegram;
+	global $telegram, $available_commands;
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
 		'text' => 'خوش آمدید'
