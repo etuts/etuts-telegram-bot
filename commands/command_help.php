6fe38bb3422c25a2fe00ba03cdcb0fbfcc52524e
sh-soltanpour <shahryar2006@gmail.com>
2017-04-22 23:40:23 +0430

    Revert Help command

--- commands/command_help.php
+++ commands/command_help.php
@@ -13,7 +13,7 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 	// $data['text'] =  "your text ".json_decode('"'.$emoticons.'"').' bla bla';
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
-		'text' => $answer.emoji('laugh'),
+		'text' => $answer,
 	]);
 	
 }
