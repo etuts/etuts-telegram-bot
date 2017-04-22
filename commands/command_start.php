22856d41b455e1a3b48d90f67f4ad800ee970da4
MohGanji <mohammad-ganji@outlook.com>
2017-04-22 18:42:39 +0430

    fixed problem with keyboard at start command

--- commands/command_start.php
+++ commands/command_start.php
@@ -2,13 +2,11 @@
 
 function run_start_command($chat_id, $text, $message_id, $message, $state) {
 	global $telegram, $available_commands, $db;
-	$telegram->sendMessage([
-		'chat_id' => $chat_id,
-		'text' => 'خوش آمدید'
-	]);
+
 	$is_admin =  $db->check_user_permission(ADMIN);
 	$is_author = $db->check_user_permission(AUTHOR);
 	$permission = $is_admin ? ADMIN : $is_author ? AUTHOR : USER;
+	
 	$commands = array();
 	foreach ($available_commands as $command) {
 		// log_debug(var_export($command, true), 117990761);
@@ -17,12 +15,19 @@ function run_start_command($chat_id, $text, $message_id, $message, $state) {
 			// $answer .= ("/".$command["name"]." - ".$command["description"]."\n");
 			array_push($commands, $command["name"]);
 	}
+	
 	$keyboard = [$commands];
-		$reply_markup = $telegram->replyKeyboardMarkup([
-			'keyboard' => $keyboard, 
-			'resize_keyboard' => true, 
-			'one_time_keyboard' => true
-		]);
+	$reply_markup = $telegram->replyKeyboardMarkup([
+		'keyboard' => $keyboard, 
+		'resize_keyboard' => true, 
+		'one_time_keyboard' => true
+	]);
+
+	$telegram->sendMessage([
+		'chat_id' => $chat_id,
+		'text' => "خوش آمدید",
+		'reply_markup' => $reply_markup
+	]);
 	// log_debug(var_export($state), 117990761);
 	//run_help_command($chat_id, $text, $message_id, $message, $state);
 }
