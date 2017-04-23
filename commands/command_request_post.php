c44e8eb52ae6271da5cc6cfaeaedaf52cdb6c4e6
Vahid Mohammadi <email@vmoh.ir>
2017-04-23 14:40:55 +0430

    add accept and deny buttons to REQUEST_POST

--- commands/command_request_post.php
+++ commands/command_request_post.php
@@ -6,7 +6,10 @@ function run_request_post_command($chat_id, $text, $message_id, $message, $state
 		case REQUEST_POST:
 			// user has sent a message to admin! Wow!!
 			send_thank_message($message_id);
-			send_message_to_admin($message, $text, 'یک درخواست جدید');
+			$btn1 = get_key_btn('rqst_acc_dny', (string)$chat_id, (string)$message_id, '"acc":1');
+			$btn2 = get_key_btn('rqst_acc_dny', (string)$chat_id, (string)$message_id, '"acc":0');
+			$reply_markup = make_keyboard([[$btn1, $btn2]]);
+			send_message_to_admin($message, $text, 'یک درخواست جدید', $reply_markup);
 			$db->reset_state();
 			break;
 		
