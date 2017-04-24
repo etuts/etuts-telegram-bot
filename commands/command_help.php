b5be80d8da437ee86e8cbea6df3d6a4afa47f711
sh-soltanpour <shahryar2006@gmail.com>
2017-04-25 01:41:22 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -24,7 +24,8 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 	$text = strip_tags($post->description);
 	$text = substr($text, 0,strlen($text)-7);
 	$testing = "[🖼](".$link.")";
-	$final_text = $post->title.$testing."\n".$text;
+	$link_to_site = $post->link;
+	$final_text = $post->title.$testing."\n".$text."\n".$link_to_site;
 
 
 	$telegram->sendMessage([
