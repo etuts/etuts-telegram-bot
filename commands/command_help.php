1fe70bf231660fb43bc36e4b2018a8f094c9a6d9
sh-soltanpour <shahryar2006@gmail.com>
2017-04-24 23:02:07 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -12,7 +12,7 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 	}
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
-		'text' => $answer,
+		'text' => $answer.emoji('laugh'),
 	]);
 	$text =get_last_post()->description.'hi';
 	$telegram->sendMessage([
