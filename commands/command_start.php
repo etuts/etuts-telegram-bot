d6887563efee3113bc3c1d8a9f7b73a88cfb67d4
Vahid Mohammadi <email@vmoh.ir>
2017-04-24 20:57:41 +0430

    debug

--- commands/command_start.php
+++ commands/command_start.php
@@ -14,7 +14,7 @@ function run_start_command($chat_id, $text, $message_id, $message, $state) {
 			if(!in_array($command["name"], $commands_to_ignore))
 				array_push($commands, $command["name"]);
 	}
-	$keyboard = oojoor($commands); // amin goft esmesho in bezar =)) baadan yeki avaz kone be couple
+	$keyboard = array_duplex($commands); // amin goft esmesho in bezar =)) baadan yeki avaz kone be couple
 									// araye intory bar migardoone:   [["0", "1"]
 									//  								["2", "3"]] ...
 	$reply_markup = $telegram->replyKeyboardMarkup([
