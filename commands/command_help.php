484b0bfe1ce5141e21c70938ec70a2404add18fe
sh-soltanpour <shahryar2006@gmail.com>
2017-04-25 01:58:39 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -24,9 +24,9 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 	$text = strip_tags($post->description);
 	// $text = substr($text, 0,strlen($text)-7);
 	$testing = "[🖼](".$link.")";
-	$link_to_site = " ";
-	$link_to_site .= $post->link;
-	$link_to_site2 = "[برای مشاهده ی این مطلب کلیک کنید](".$link_to_site.")";
+	// $link_to_site = " ";
+	$link_to_site = $post->link;
+	$link_to_site = "[برای مشاهده ی این مطلب کلیک کنید](".$link_to_site.")";
 	$final_text = $post->title.$testing."\n".$text."\n".$link_to_site2;
 
 
