35fd36bfcb808cbfcd974f5c4c98fbc9bd8fd7b2
sh-soltanpour <shahryar2006@gmail.com>
2017-04-25 22:41:01 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -34,6 +34,6 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
     $telegram->sendMessage([
         'chat_id' => $chat_id,
         'text' => $author,
-        'parse_mode' => "Markdown",
+        // 'parse_mode' => "Markdown",
     ]);
 }
