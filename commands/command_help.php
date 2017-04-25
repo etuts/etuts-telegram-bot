64b7256942f591eca533bef28ad03cc4df261d89
sh-soltanpour <shahryar2006@gmail.com>
2017-04-25 22:39:47 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -14,4 +14,26 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 		'chat_id' => $chat_id,
 		'text' => $answer,
 	]);
+	$text = "";
+    $post = get_last_post();
+    $text .= $post->description;
+    $pos = strpos($text, "src=\"") + 5;
+    $text = substr($text,$pos);
+    $pos2 = strpos($text, "\"");
+    $link = substr($text,0,$pos2);
+    $text = strip_tags($post->description);
+    $text = substr($text, 0,strlen($text)-9);
+    $image_link = "[".emoji('image-icon')."](".$link.")";
+    $author = $post->creator;
+    // $link_to_site = " ";
+    $link_to_site = $post->link;
+    $link_to_site = "[برای مشاهده ی مطلب کلیک کنید](".$link_to_site.")";
+    $final_text = $post->title.$image_link."\n".$text."\n".$link_to_site."\n".$author;
+
+
+    $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => $final_text,
+        'parse_mode' => "Markdown",
+    ]);
 }
