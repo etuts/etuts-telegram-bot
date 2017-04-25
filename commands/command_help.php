94e91d71f14ec309c69fa4a3cc0d229e708f1fde
sh-soltanpour <shahryar2006@gmail.com>
2017-04-26 02:08:43 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -25,16 +25,18 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 	]);
 	$params =['user_id' => $chat_id];
 	$photos = $telegram->getUserProfilePhotos($params);
-	$telegram->sendMessage([
-		'chat_id' => $chat_id,
-		'text' => "test",
-		]);
 	$photo = $photos[0][0];
 	if ($photo != null){
 		$file = $telegram->getFile($photo->file_id);
 		$telegram->sendMessage([
 		'chat_id' => $chat_id,
 		'text' => $file->file_path,
+		]);
+	}
+	else{
+		$telegram->sendMessage([
+		'chat_id' => $chat_id,
+		'text' => "else",
 		]);
 	}
 	
