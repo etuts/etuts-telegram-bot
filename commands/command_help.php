fd912abf9172faa45ab442466f18b51e20002835
sh-soltanpour <shahryar2006@gmail.com>
2017-04-25 22:43:37 +0430

    Test

--- commands/command_help.php
+++ commands/command_help.php
@@ -16,6 +16,7 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 	]);
 	$text = "";
     $post = get_last_post();
+    print_r($author);
     $text .= $post->description;
     $pos = strpos($text, "src=\"") + 5;
     $text = substr($text,$pos);
@@ -33,7 +34,7 @@ function run_help_command($chat_id, $text, $message_id, $message, $state) {
 
     $telegram->sendMessage([
         'chat_id' => $chat_id,
-        'text' => $author,
-        // 'parse_mode' => "Markdown",
+        'text' => $final_text,
+        'parse_mode' => "Markdown",
     ]);
 }
