9c53b76d13550dfdd91eda667a45e74961eae587
MohGanji <mohammad-ganji@outlook.com>
2017-04-29 02:31:18 +0430

    updating accept-deny to edit the message after answer.

--- callbackqueries/callback_request_accept_deny.php
+++ callbackqueries/callback_request_accept_deny.php
@@ -6,6 +6,11 @@ function callback_rqst_acc_dny($id, $from, $message, $data) {
 	$message_id = $data['m'];
 	$accepted = $data['acc'];
 	
+	$admin_msg_id = $message->getMessageId();
+	$admin_chat_id = $message->getChatId();
+	log_debug($admin_msg_id, 117990761);
+	log_debug($admin_chat_id, 117990761);
+	
 	$user_answer;
 	$admin_answer;
 
@@ -16,6 +21,13 @@ function callback_rqst_acc_dny($id, $from, $message, $data) {
 		$user_answer = 'مطلب درخواستی شما تایید نشد';
 		$admin_answer = 'رد شد';
 	}
+	// editing the message to remove the buttons.
+	$telegram->editMessageReplyMarkup([
+		'chat_id' => $admin_chat_id,
+		'message_id' => $admin_msg_id,
+		'reply_markup' => '',
+	]);
+
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
 		'text' => $user_answer,
