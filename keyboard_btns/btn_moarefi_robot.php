375f58fb53dcfc0709d5043d84d0b6c9daa56b30
Vahid Mohammadi <email@vmoh.ir>
2017-04-29 02:47:43 +0430

    debug

--- keyboard_btns/btn_moarefi_robot.php
+++ keyboard_btns/btn_moarefi_robot.php
@@ -4,7 +4,7 @@ function btn_moarefi_robot($chat_id, $text, $message_id, $message, $state) {
 	global $db;
 	switch ($state) {
 		case MOAREFI_ROBOT_SCHEDULE_POST:
-			$data = json_decode($db->get_data(), true);
+			$data = $db->get_data();
 
 			$image = ($message->isType('photo')) ? $message->getPhoto() : false;
 			$final_text = make_post_moarefi_robot_for_channel($data['bot_id'], $image, $data['title'], $data['description']);
@@ -14,25 +14,26 @@ function btn_moarefi_robot($chat_id, $text, $message_id, $message, $state) {
 			send_thank_message($message_id);
 			break;
 		case MOAREFI_ROBOT_BOT_IMAGE:
-			if (strlen($text))
-			$data = json_decode($db->get_data(), true);
+			// if (strlen($text))
+			$data = $db->get_data();
 			$data['description'] = $text;
-			$db->set_data(json_encode($data));
+			$db->set_data($data);
 
 			reply('اگر برای معرفی این ربات عکسی دارید بفرستید وگرنه عبارت no را تایپ کنید', $message_id, true);
 			$db->set_state(MOAREFI_ROBOT_SCHEDULE_POST);
 			break;
 		case MOAREFI_ROBOT_BOT_DESCRIPTION:
-			$data = json_decode($db->get_data(), true);
+			$data = $db->get_data();
 			$data['bot_id'] = $text;
-			$db->set_data(json_encode($data));
+
+			$db->set_data($data);
 			
 			reply('توضیحات مربوط به ربات رو وارد کنید', $message_id, true);
 			$db->set_state(MOAREFI_ROBOT_BOT_IMAGE);
 			break;
 		case MOAREFI_ROBOT_BOT_ID:
 			$data['title'] = $text;
-			$db->set_data(json_encode($data));
+			$db->set_data($data);
 
 			reply('آی دی ربات رو با @ وارد کن', $message_id, true);
 			$db->set_state(MOAREFI_ROBOT_BOT_DESCRIPTION);
@@ -53,11 +54,11 @@ function make_post_moarefi_robot_for_channel($bot_id, $bot_image, $title, $descr
 					$description . "\n" .
 					"@etuts" . "\n" .
 					"#telegram #bot";
-
+	
 	if ($bot_image == false) {
 		$final_text = [
 			'type' => 'text',
-			'chat_id' => $chat_id,
+			'chat_id' => 92454,
 			'text' => $text,
 		];
 	} else {
@@ -69,5 +70,5 @@ function make_post_moarefi_robot_for_channel($bot_id, $bot_image, $title, $descr
 			'caption' => $text,
 		];
 	}
-	return json_encode($final_text);
+	return addslashes(json_encode($final_text));
 }
