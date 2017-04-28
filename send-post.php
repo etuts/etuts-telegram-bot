fa2e60a96604da8f7f1dcc1cb9b4c7388ce82be3
Vahid Mohammadi <email@vmoh.ir>
2017-04-29 01:39:29 +0430

    works with data base now

--- send-post.php
+++ send-post.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php 
 /*
-open a file that contains posts. each post on a line
+open a db that contains posts. each post on a line
 send the post to channel (you may be able to require the codes of iraqsaqi (bayad beshe dige moshkeli nadare))
 delete that line
 */
@@ -10,32 +10,27 @@ require __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/config.php';
 use Telegram\Bot\Api;
 
-require __DIR__.'/posts_file_class.php';
+require __DIR__.'/database_class.php';
 
-$file = new Posts_file(false);
+$db = new Database($db_name, $db_user, $db_pass);
+$telegram = new Api($token);
 
-if ($file) {
-	$telegram = new Api($token);
-
-	$post_data = $file->read_post();
-	$type = $post_data['type'];
-	if ($type == '')
-		die();
-	switch ($type) {
-		case 'text':
-			$telegram->sendMessage([
-				'chat_id' => $post_data['chat_id'],
-				'text' => utf8_decode($post_data['text']),
-			]);
-			break;
-		case 'photo':
-			$telegram->sendPhoto([
-				'chat_id' => $post_data['chat_id'],
-				'photo' => $post_data['photo'],
-				'caption' => utf8_decode($post_data['caption']),
-			]);
-			break;
-	}
-} else
-	echo 'failed' . PHP_EOL;
-
+$post_data = $db->read_post();
+$type = $post_data['type'];
+if ($type == '')
+	die();
+switch ($type) {
+	case 'text':
+		$telegram->sendMessage([
+			'chat_id' => $post_data['chat_id'],
+			'text' => utf8_decode($post_data['text']),
+		]);
+		break;
+	case 'photo':
+		$telegram->sendPhoto([
+			'chat_id' => $post_data['chat_id'],
+			'photo' => $post_data['photo'],
+			'caption' => utf8_decode($post_data['caption']),
+		]);
+		break;
+}
