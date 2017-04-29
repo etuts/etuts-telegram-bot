a7f6945d0a491d6ffb70130526484b665c4c4bb4
Vahid Mohammadi <email@vmoh.ir>
2017-04-30 01:20:13 +0430

    debug

--- commands/command_remove_last_post.php
+++ commands/command_remove_last_post.php
@@ -1,6 +1,7 @@
 <?php
 
-function remove_last_channel_post($chat_id, $text, $message_id, $message, $state) {
+function run_remove_last_channel_post_command($chat_id, $text, $message_id, $message, $state) {
+	global $db;
 	$db->remove_last_channelpost();
 	send_thank_message();
 }
