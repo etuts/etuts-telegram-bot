e06a1b5290c73e6b5382eedf1de21345866ffaef
MohGanji <mohammad-ganji@outlook.com>
2017-04-29 17:27:22 +0430

    added categories button function

--- keyboard_btns/btn_commands_btns.php
+++ keyboard_btns/btn_commands_btns.php
@@ -15,3 +15,7 @@ function btn_schedule_post($chat_id, $text, $message_id, $message, $state) {
 function btn_post_validation($chat_id, $text, $message_id, $message, $state) {
 	run_post_validation_command($chat_id, $text, $message_id, $message, $state);
 }
+
+function btn_categories($chat_id, $text, $message_id, $message, $state){
+	run_categories_command($chat_id, $text, $message_id, $message, $state);
+}
