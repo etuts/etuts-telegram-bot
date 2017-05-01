ee296aac0624c638bc9fa4cd2de377eed68bcd08
sh-soltanpour <shahryar2006@gmail.com>
2017-05-01 20:55:17 +0430

    Debug

--- utilities.php
+++ utilities.php
@@ -63,7 +63,7 @@ function get_last_topic(){
 	file_put_contents("feed", fopen("http://etuts.ir/topics/feed", 'r'));
 	$rss = simplexml_load_file('feed');
 	$last_item = $rss->channel->title;
-	return $rss;
+	return $last_item;
 }
 function make_post_for_channel($title, $description, $image_link = false, $link_to_site = false) {
     
