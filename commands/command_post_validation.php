6aad49ed71a3c41a4dafb9b79b9c0fd5a729505c
Vahid Mohammadi <email@vmoh.ir>
2017-05-04 15:32:16 +0430

    move author and admin check to handle_commands.php

--- commands/command_post_validation.php
+++ commands/command_post_validation.php
@@ -14,13 +14,9 @@ function run_post_validation_command($chat_id, $text, $message_id, $message, $st
 			break;
 		
 		default:
-			if ($db->check_user_permission(AUTHOR) || $db->check_user_permission(ADMIN)) {
-				$db->set_state(POST_VALIDATION_SEND_POST_TITLE);
-				$answer = 'عنوان مطلب و لینک مطلبی که می خواهید بنویسید را وارد کنید';
-				$reply_markup = $telegram->forceReply();
-			} else {
-				$answer = 'ببخشید اما شما نویسنده ی سایت نیستید!';
-			}
+			$db->set_state(POST_VALIDATION_SEND_POST_TITLE);
+			$answer = 'عنوان مطلب و لینک مطلبی که می خواهید بنویسید را وارد کنید';
+			$reply_markup = $telegram->forceReply();
 			$telegram->sendMessage([
 				'chat_id' => $chat_id,
 				'text' => $answer,
