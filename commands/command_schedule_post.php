eb3e918d55f1c83dc60fb1cac889595ab497da76
Vahid Mohammadi <email@vmoh.ir>
2017-05-04 15:32:41 +0430

    move author and admin check to handle_commands.php

--- commands/command_schedule_post.php
+++ commands/command_schedule_post.php
@@ -2,17 +2,13 @@
 
 function run_schedule_post_command($chat_id, $text, $message_id, $message, $state) {
 	global $telegram, $db;
-	if ($db->check_user_permission(ADMIN)) {
-		$answer = 'نوع مطلبی که میخوای بفرستی رو مشخص کن' . PHP_EOL;
-		$keyboard = [['معرفی ربات', 'معرفی ابزار']];
-		$reply_markup = $telegram->replyKeyboardMarkup([
-			'keyboard' => $keyboard, 
-			'resize_keyboard' => true, 
-			'one_time_keyboard' => true
-		]);
-	} else {
-		$answer = 'برای استفاده از این دستور باید ادمین کانال باشید';
-	}
+	$answer = 'نوع مطلبی که میخوای بفرستی رو مشخص کن' . PHP_EOL;
+	$keyboard = [['معرفی ربات', 'معرفی ابزار']];
+	$reply_markup = $telegram->replyKeyboardMarkup([
+		'keyboard' => $keyboard, 
+		'resize_keyboard' => true, 
+		'one_time_keyboard' => true
+	]);
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
 		'text' => $answer,
