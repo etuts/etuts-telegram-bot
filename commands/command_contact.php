0964efa1d2c15e9742b6c3495012afb3712b898a
Vahid Mohammadi <email@vmoh.ir>
2017-05-04 16:26:37 +0430

    use reply function

--- commands/command_contact.php
+++ commands/command_contact.php
@@ -35,13 +35,7 @@ function run_contact_command($chat_id, $text, $message_id, $message, $state) {
 			break;
 		default:
 			$db->set_state(CONTACT);
-			$reply_markup = $telegram->forceReply();
-			$telegram->sendMessage([
-				'chat_id' => $chat_id,
-				'text' => 'لطفا پیام تان را بفرستید',
-				'reply_to_message_id' => $message_id,
-				'reply_markup' => $reply_markup
-			]);
+			reply('لطفا پیام تان را بفرستید', true);
 			break;
 	}
 }
