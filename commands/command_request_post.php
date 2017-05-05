07ceb10525adfe8826f913487c733fa0fcabb691
Vahid Mohammadi <email@vmoh.ir>
2017-05-06 02:09:51 +0430

    new reset_state

--- commands/command_request_post.php
+++ commands/command_request_post.php
@@ -8,9 +8,8 @@ function run_request_post_command($chat_id, $text, $message_id, $message, $state
 			$btn1 = create_glassy_btn(emoji('like'), 'rqst_acc_dny', ['c' => $chat_id, 'm' => $message_id, 'acc' => 1]);
 			$btn2 = create_glassy_btn(emoji('dislike'), 'rqst_acc_dny', ['c' => $chat_id, 'm' => $message_id, 'acc' => 0]);
 			$reply_markup = create_glassy_keyboard([[$btn1, $btn2]]);
-			send_message_to_admin($message, $text, 'یک درخواست جدید', $reply_markup);
-			$db->reset_state();
-			send_thank_message($message_id);
+			send_message_to_admin(create_report_from_a_user_message('یک درخواست جدید', $text), $reply_markup);
+			reset_state(THANK_MESSAGE);
 			break;
 		
 		default:
