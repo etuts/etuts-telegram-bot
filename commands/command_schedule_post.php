b944afc27865cb9e03451ee72cf25bc9e35e5389
Vahid Mohammadi <email@vmoh.ir>
2017-05-05 16:14:35 +0430

    debug

--- commands/command_schedule_post.php
+++ commands/command_schedule_post.php
@@ -1,17 +1,16 @@
 <?php
 
 function run_schedule_post_command($chat_id, $text, $message_id, $message, $state) {
-	global $telegram, $db;
-	$answer = 'نوع مطلبی که میخوای بفرستی رو مشخص کن';
+	global $telegram;
 	$keyboard = [['معرفی ربات', 'معرفی ابزار']];
 	$reply_markup = $telegram->replyKeyboardMarkup([
-		'keyboard' => $keyboard, 
-		'resize_keyboard' => true, 
+		'keyboard' => $keyboard,
+		'resize_keyboard' => true,
 		'one_time_keyboard' => true
 	]);
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
-		'text' => $answer,
+		'text' => 'نوع مطلبی که میخوای بفرستی رو مشخص کن',
 		'reply_markup' => $reply_markup
 	]);
 }
