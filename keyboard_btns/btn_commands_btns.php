b766fb35cecfc660c0704744d7b21945e4879069
Vahid Mohammadi <email@vmoh.ir>
2017-05-06 02:37:43 +0430

    new start keyboard buttons

--- keyboard_btns/btn_commands_btns.php
+++ keyboard_btns/btn_commands_btns.php
@@ -19,3 +19,15 @@ function btn_post_validation($chat_id, $text, $message_id, $message, $state) {
 function btn_categories($chat_id, $text, $message_id, $message, $state){
 	run_categories_command($chat_id, $text, $message_id, $message, $state);
 }
+
+function btn_remove_last_channelpost($chat_id, $text, $message_id, $message, $state){
+	run_remove_last_channel_post_command($chat_id, $text, $message_id, $message, $state);
+}
+
+function btn_post_source($chat_id, $text, $message_id, $message, $state){
+	run_post_source_command($chat_id, $text, $message_id, $message, $state);
+}
+
+function btn_get_site_recommend_post($chat_id, $text, $message_id, $message, $state){
+	run_get_site_recommend_post_command($chat_id, $text, $message_id, $message, $state);
+}
