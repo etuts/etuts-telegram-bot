cee08a2fc25b73e6837c5fad27b30e7af22daa50
Vahid Mohammadi <email@vmoh.ir>
2017-05-05 20:49:50 +0430

    edit message text

--- callbackqueries/callback_user_acquire_post.php
+++ callbackqueries/callback_user_acquire_post.php
@@ -5,13 +5,19 @@ function callback_usr_acuir_post($id, $from, $message, $data) {
 
 	$glassy_msg_id = $message->getMessageId();
 	$glassy_chat_id = $message->getChat()->getId();
+	$glassy_text = $message->getText();
 
 	$db->set_site_recommend_first_post_state(RESERVED);
 	
 	// editing the message to remove the buttons.
-	$telegram->editMessageReplyMarkup([
+	/*$telegram->editMessageReplyMarkup([
 		'chat_id' => $glassy_chat_id,
 		'message_id' => $glassy_msg_id,
+	]);*/
+	$telegram->editMessageText([
+		'chat_id' => $glassy_chat_id,
+		'message_id' => $glassy_msg_id,
+		'text' => $glassy_text . PHP_EOL . PHP_EOL . emoji('alert') . 'شما این مطلب را برای نوشتن انتخاب کرده اید',
 	]);
 
 	$db->reset_state();
