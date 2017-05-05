d733c4f65554f29191025362028469ccb2d0f671
Vahid Mohammadi <email@vmoh.ir>
2017-05-06 01:59:48 +0430

    if $post === false

--- commands/command_get_recommended_post.php
+++ commands/command_get_recommended_post.php
@@ -3,6 +3,11 @@
 function run_get_recommended_post_command($chat_id, $text, $message_id, $message, $state) {
 	global $db, $telegram;
 	$post = $db->get_site_recommend_post();
+	if ($post === false) {
+		reply('هیچ مطلبی برای پیشنهاد وجود ندارد');
+		send_message_to_admin('هیچ مطلبی برای پیشنهاد وجود ندارد');
+		return;
+	}
 	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post');
 	$reply_markup = create_glassy_keyboard([[$btn]]);
 	$telegram->sendMessage([
