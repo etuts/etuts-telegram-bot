b60240f36d42e0421180576a257a4c56ccff8c52
Vahid Mohammadi <email@vmoh.ir>
2017-05-06 02:09:13 +0430

    new reset_state

--- commands/command_cancel.php
+++ commands/command_cancel.php
@@ -2,12 +2,5 @@
 
 function run_cancel_command($chat_id, $text, $message_id, $message, $state) {
 	global $telegram, $db;
-	$db->reset_state();
-	$reply_markup = $telegram->replyKeyboardHide();
-	$telegram->sendMessage([
-		'chat_id' => $chat_id,
-		'text' => 'عملیات با موفقیت کنسل شد',
-		'reply_to_message_id' => $message_id,
-		'reply_markup' => $reply_markup
-	]);
+	reset_state('عملیات با موفقیت کنسل شد');
 }
