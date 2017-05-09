7668c55c02b586d7676a28ecc65213c69bd379e2
Vahid Mohammadi <email@vmoh.ir>
2017-05-10 01:39:56 +0430

    remove commands to ignore from get_initial_keyboard

--- telegram_helpers.php
+++ telegram_helpers.php
@@ -147,12 +147,12 @@ function get_initial_keyboard() {
 	$permission = $is_admin ? ADMIN : $is_author ? AUTHOR : USER;
 	$buttons = $keyboard_buttons["start"];
 	$commands = array();
-	$commands_to_ignore = array("help", "cancel", "start");
+	// $commands_to_ignore = array("help", "cancel", "start");
 	
 	foreach ($buttons as $command) {
 		if ($command["permission"] <= $permission)
-			if(!in_array($command["name"], $commands_to_ignore))
-				array_push($commands, $command["name"]);
+			// if(!in_array($command["name"], $commands_to_ignore))
+			array_push($commands, $command["name"]);
 	}
 
 	$keyboard = array_duplex($commands);
