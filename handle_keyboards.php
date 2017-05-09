d64aa6a9041d5fce15a280cfc9800838a9cea05a
Vahid Mohammadi <email@vmoh.ir>
2017-05-10 02:16:32 +0430

    add manage_channel_posts keybaord button

--- handle_keyboards.php
+++ handle_keyboards.php
@@ -14,6 +14,7 @@ $keyboard_buttons = [
 		"remove_last_channelpost"=>array("name"=>"حذف آخرین مطلب زمان بندی شده کانال", "permission"=>ADMIN),
 		"post_source"=>array("name"=>"پیشنهاد مطلب به نویسنده ها", "permission"=>ADMIN),
 		"get_site_recommend_post"=>array("name"=>"مطلب برای نوشتن تو سایت", "permission"=>AUTHOR),
+		"manage_channel_posts"=>array("name"=>"مدیریت مطالب زمانبندی شده کانال", "permission"=>ADMIN),
 	],
 
 ];
