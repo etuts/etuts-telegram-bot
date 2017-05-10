d7d3ae415c9c0468f41b1f8a038e8cadacc35739
sh-soltanpour <shahryar2006@gmail.com>
2017-05-10 16:00:30 +0430

    Test

--- send-last-rss.php
+++ send-last-rss.php
@@ -26,6 +26,7 @@ function get_last_post(){
     return $last_item;
 }
 function get_last_topic(){
+    log_debug("entered functuon",97778738);
 	file_put_contents("feed", fopen("http://etuts.ir/topics/feed", 'r'));
 	$rss = simplexml_load_file('feed');
 	$last_item = $rss->channel->item;
