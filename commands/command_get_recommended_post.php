7c0b48b33cad70f0e9317df971f2b760d32ea992
Vahid Mohammadi <email@vmoh.ir>
2017-05-12 01:31:08 +0430

    add toggle button

--- commands/command_get_recommended_post.php
+++ commands/command_get_recommended_post.php
@@ -8,7 +8,8 @@ function run_get_recommended_post_command($chat_id, $text, $message_id, $message
 		send_message_to_admin('هیچ مطلبی برای پیشنهاد وجود ندارد');
 		return;
 	}
-	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post');
+	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED]);
+	$btn2 = create_glassy_btn('یک مطلب دیگه پیشنهاد بده', 'gt_anthr_post', ['i' => 1]);
 	$reply_markup = create_glassy_keyboard([[$btn]]);
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
