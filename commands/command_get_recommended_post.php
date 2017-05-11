507278dff63bc425e2d628dc61cc4ba03579f76f
Vahid Mohammadi <email@vmoh.ir>
2017-05-12 02:04:49 +0430

    debug

--- commands/command_get_recommended_post.php
+++ commands/command_get_recommended_post.php
@@ -8,7 +8,7 @@ function run_get_recommended_post_command($chat_id, $text, $message_id, $message
 		send_message_to_admin('هیچ مطلبی برای پیشنهاد وجود ندارد');
 		return;
 	}
-	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED]);
+	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED, 'i' => 1]);
 	$btn2 = create_glassy_btn('یک مطلب دیگه پیشنهاد بده', 'gt_anthr_post', ['i' => 1]);
 	$reply_markup = create_glassy_keyboard([[$btn], [$btn2]]);
 	$telegram->sendMessage([
