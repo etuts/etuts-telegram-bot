a7725a30cb3dd9f5f69a52ec999b5211ef492832
Vahid Mohammadi <email@vmoh.ir>
2017-05-12 01:29:37 +0430

    add text to each state of toggle button

--- callbackqueries/callback_user_acquire_post.php
+++ callbackqueries/callback_user_acquire_post.php
@@ -10,17 +10,21 @@ function callback_usr_acuir_post($id, $from, $message, $data) {
 	$reserved = $data['r'];
 
 	$db->set_site_recommend_first_post_state($reserved);
-	if ($reserved)
+	if ($reserved) {
 		$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED]);
-	else
+		$text = $glassy_text . PHP_EOL . PHP_EOL . emoji('alert') . ' توجه: شما این مطلب را برای نوشتن انتخاب کرده اید';
+	}
+	else {
 		$btn = create_glassy_btn('پشیمون شدم! نمیتونم تهیه ش کنم', 'usr_acuir_post', ['r' => NOT_RESERVED]);
+		$text = substr($text, 0, strrpos($text, "\n")); // remove last line from text
+	}
 	$reply_markup = create_glassy_keyboard([[$btn]]);
 	
 	// editing the message to remove the buttons.
 	$telegram->editMessageText([
 		'chat_id' => $glassy_chat_id,
 		'message_id' => $glassy_msg_id,
-		'text' => $glassy_text . PHP_EOL . PHP_EOL . emoji('alert') . ' توجه: شما این مطلب را برای نوشتن انتخاب کرده اید',
+		'text' => $text,
 		'reply_markup' => $reply_markup,
 	]);
 
