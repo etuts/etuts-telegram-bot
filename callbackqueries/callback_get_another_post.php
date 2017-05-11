de1de21369183a4aeb39940c4908525b6108ad37
Vahid Mohammadi <email@vmoh.ir>
2017-05-12 01:35:24 +0430

    add buttons

--- callbackqueries/callback_get_another_post.php
+++ callbackqueries/callback_get_another_post.php
@@ -7,13 +7,18 @@ function callback_gt_anthr_post($id, $from, $message, $data) {
 	$glassy_chat_id = $message->getChat()->getId();
 	$glassy_text = $message->getText();
 
-	$post = $db->get_site_recommend_post($data['i']);
+	$new_index = $data['i']+1;
+	$post = $db->get_site_recommend_post();
+	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED]);
+	$btn2 = create_glassy_btn('یک مطلب دیگه پیشنهاد بده', 'gt_anthr_post', ['i' => $new_index]);
+	$reply_markup = create_glassy_keyboard([[$btn], [$btn2]]);
 	
 	// editing the message to remove the buttons.
 	$telegram->editMessageText([
 		'chat_id' => $glassy_chat_id,
 		'message_id' => $glassy_msg_id,
 		'text' => 'مطلب پیشنهاد شده برای نوشتن در سایت:' . PHP_EOL . PHP_EOL . $post,
+		'reply_markup' => $reply_markup,
 	]);
 
 	$answer_data = ['text' => 'مطلب جدید'];
