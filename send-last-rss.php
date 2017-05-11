55ad810f95aa08795db83e8d2b089766e1e1add9
Vahid Mohammadi <email@vmoh.ir>
2017-05-12 01:10:54 +0430

    debug

--- send-last-rss.php
+++ send-last-rss.php
@@ -14,7 +14,7 @@ require __DIR__.'/database_class.php';
 
 $db = new Database($db_name, $db_user, $db_pass);
 $telegram = new Api($token);
-file_put_contents("feed", fopen("http://etuts.ir/feed", 'r'));
+
 send_last_post_to_users();
 // display_latest_post(97778738);
 // $rss = get_last_post();
