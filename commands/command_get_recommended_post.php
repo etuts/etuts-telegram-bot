7928a0d8ef3e1f2c0566d7d632c34cdfcf4716b4
Vahid Mohammadi <email@vmoh.ir>
2017-05-12 01:34:00 +0430

    debug

--- commands/command_get_recommended_post.php
+++ commands/command_get_recommended_post.php
@@ -10,7 +10,7 @@ function run_get_recommended_post_command($chat_id, $text, $message_id, $message
 	}
 	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED]);
 	$btn2 = create_glassy_btn('یک مطلب دیگه پیشنهاد بده', 'gt_anthr_post', ['i' => 1]);
-	$reply_markup = create_glassy_keyboard([[$btn]]);
+	$reply_markup = create_glassy_keyboard([[$btn], [$btn2]]);
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
 		'text' => 'مطلب پیشنهاد شده برای نوشتن در سایت:' . PHP_EOL . PHP_EOL . $post,
