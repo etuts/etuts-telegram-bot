085932111d8391fdba925ee32e47d632f9a70481
Vahid Mohammadi <email@vmoh.ir>
2017-05-12 01:38:38 +0430

    debug

--- callbackqueries/callback_get_another_post.php
+++ callbackqueries/callback_get_another_post.php
@@ -8,7 +8,7 @@ function callback_gt_anthr_post($id, $from, $message, $data) {
 	$glassy_text = $message->getText();
 
 	$new_index = $data['i']+1;
-	$post = $db->get_site_recommend_post();
+	$post = $db->get_site_recommend_post($new_index-1);
 	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED]);
 	$btn2 = create_glassy_btn('یک مطلب دیگه پیشنهاد بده', 'gt_anthr_post', ['i' => $new_index]);
 	$reply_markup = create_glassy_keyboard([[$btn], [$btn2]]);
