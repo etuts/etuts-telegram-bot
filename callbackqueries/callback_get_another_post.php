ff2a3fc2ad0372a0fcfbf716ddd2a0ad81049cc1
Vahid Mohammadi <email@vmoh.ir>
2017-05-12 01:42:37 +0430

    add to states and texts and buttons

--- callbackqueries/callback_get_another_post.php
+++ callbackqueries/callback_get_another_post.php
@@ -9,15 +9,23 @@ function callback_gt_anthr_post($id, $from, $message, $data) {
 
 	$new_index = $data['i']+1;
 	$post = $db->get_site_recommend_post($new_index-1);
-	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED]);
+
+	if ($post === false) {
+		$btn3 = create_glassy_btn('نمیاش اولین مطلب پیشنهادی', 'gt_anthr_post', ['i' => 0]);
+		$reply_markup = create_glassy_keyboard([[$btn3]]);
+		$text = 'به پایان مطالب پیشنهادی رسیدیم!';
+	} else {
+		$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED]);
 	$btn2 = create_glassy_btn('یک مطلب دیگه پیشنهاد بده', 'gt_anthr_post', ['i' => $new_index]);
-	$reply_markup = create_glassy_keyboard([[$btn], [$btn2]]);
+		$reply_markup = create_glassy_keyboard([[$btn], [$btn2]]);
+		$text = 'مطلب پیشنهاد شده برای نوشتن در سایت:' . PHP_EOL . PHP_EOL . $post;
+	}
 	
 	// editing the message to remove the buttons.
 	$telegram->editMessageText([
 		'chat_id' => $glassy_chat_id,
 		'message_id' => $glassy_msg_id,
-		'text' => 'مطلب پیشنهاد شده برای نوشتن در سایت:' . PHP_EOL . PHP_EOL . $post,
+		'text' => $text,
 		'reply_markup' => $reply_markup,
 	]);
 
