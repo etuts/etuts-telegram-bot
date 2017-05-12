e6a8089af8d01dd773d414666d01da683b5c9148
Vahid Mohammadi <email@vmoh.ir>
2017-05-12 15:52:57 +0430

    debug

--- callbackqueries/callback_get_another_post.php
+++ callbackqueries/callback_get_another_post.php
@@ -8,14 +8,15 @@ function callback_gt_anthr_post($id, $from, $message, $data) {
 	$glassy_text = $message->getText();
 
 	$row = $db->get_site_recommend_post($data['i']);
-	$id = $row['id'];
-	$post = $row['post'];
 
-	if ($post === false) {
+	// log_debug(var_export($row, true));
+	if ($row === false) {
 		$btn3 = create_glassy_btn('نمایش اولین مطلب پیشنهادی', 'gt_anthr_post', ['i' => 0]);
 		$reply_markup = create_glassy_keyboard([[$btn3]]);
 		$text = 'به پایان مطالب پیشنهادی رسیدیم!';
 	} else {
+		$id = $row['id'];
+		$post = $row['post'];
 		$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED, 'i' => $id]);
 		$btn2 = create_glassy_btn('یک مطلب دیگه پیشنهاد بده', 'gt_anthr_post', ['i' => $id]);
 		$reply_markup = create_glassy_keyboard([[$btn], [$btn2]]);
