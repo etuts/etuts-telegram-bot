d4fcff0ed55c743722cb0b69e4e1ab59be2bc971
Vahid Mohammadi <email@vmoh.ir>
2017-05-12 15:25:34 +0430

    add $index

--- commands/command_get_recommended_post.php
+++ commands/command_get_recommended_post.php
@@ -2,14 +2,16 @@
 
 function run_get_recommended_post_command($chat_id, $text, $message_id, $message, $state) {
 	global $db, $telegram;
-	$post = $db->get_site_recommend_post();
+	$row = $db->get_site_recommend_post();
+	$post = $row['post'];
+	$index = $row['index'];
 	if ($post === false) {
 		reply('هیچ مطلبی برای پیشنهاد وجود ندارد');
 		send_message_to_admin('هیچ مطلبی برای پیشنهاد وجود ندارد');
 		return;
 	}
-	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED, 'i' => 1]);
-	$btn2 = create_glassy_btn('یک مطلب دیگه پیشنهاد بده', 'gt_anthr_post', ['i' => 1]);
+	$btn = create_glassy_btn('این مطلب را تهیه میکنم', 'usr_acuir_post', ['r' => RESERVED, 'i' => $index]);
+	$btn2 = create_glassy_btn('یک مطلب دیگه پیشنهاد بده', 'gt_anthr_post', ['i' => $index]);
 	$reply_markup = create_glassy_keyboard([[$btn], [$btn2]]);
 	$telegram->sendMessage([
 		'chat_id' => $chat_id,
