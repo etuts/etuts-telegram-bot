52890cb33348ccf77651af21805e11db836bfbc2
sh-soltanpour <shahryar2006@gmail.com>
2017-05-14 01:03:56 +0430

    send-last-topic file added

---
+++ send-last-topic.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php
+<?php 
+/*
+open a db that contains posts. each post on a line
+send the post to channel (you may be able to require the codes of iraqsaqi (bayad beshe dige moshkeli nadare))
+delete that line
+*/
+require __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/config.php';
+use Telegram\Bot\Api;
+require __DIR__.'/database_class.php';
+
+send_last_topic(97778738);
+function get_last_topic(){
+	file_put_contents("feed", fopen("http://etuts.ir/topics/feed", 'r'));
+	$rss = simplexml_load_file('feed');
+	$last_item = $rss->channel->item;
+	return $last_item;
+}
+function send_last_topic($chat_id){
+	$topic = get_last_topic();
+	$text = "".$topic->title;
+	$telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => $text,
+    ]);
+}
