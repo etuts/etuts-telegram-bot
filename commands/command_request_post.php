5241e10d7905afd54caecad938bea4b83cf5fd04
Vahid Mohammadi <email@vmoh.ir>
2017-05-15 01:08:57 +0430

    debug

--- commands/command_request_post.php
+++ commands/command_request_post.php
@@ -5,8 +5,8 @@ function run_request_post_command($chat_id, $text, $message_id, $message, $state
 	switch ($state) {
 		case REQUEST_POST:
 			// user has sent a message to admin! Wow!!
-			$btn1 = create_glassy_btn(emoji('like'), 'rqst_acc_dny', ['c' => $chat_id, 'm' => $message_id, 'acc' => 1]);
-			$btn2 = create_glassy_btn(emoji('dislike'), 'rqst_acc_dny', ['c' => $chat_id, 'm' => $message_id, 'acc' => 0]);
+	$btn1 = create_glassy_btn(emoji('like'), 'rqst_acc_dny', []);
+	$btn2 = create_glassy_btn(emoji('dislike'), 'rqst_acc_dny', []);
 			$reply_markup = create_glassy_keyboard([[$btn1, $btn2]]);
 			send_message_to_admin(create_report_from_a_user_message('یک درخواست جدید', $text), $reply_markup);
 			reset_state(THANK_MESSAGE);
