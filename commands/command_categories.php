734720814a31a6e5711c9153564ec414cf071796
Vahid Mohammadi <email@vmoh.ir>
2017-05-15 18:23:23 +0430

    use the new base_command

--- commands/command_categories.php
+++ commands/command_categories.php
@@ -1,10 +1,11 @@
 <?php 
 
-	function run_categories_command($chat_id, $text, $message_id, $message, $state){
-		global $telegram, $db;
+class categories_command extends base_command {
+	public $name = 'categories';
+	public $description = 'انتخاب دسته بندی ها برای دریافت آخرین مطالب سایت';
 
-		// $checked = db->get_categories_checked_array(); // this must be taken from database to see if it is checked or not.
-		// temp:
+	function run($chat_id, $text, $message_id, $message, $state){
+		global $telegram, $db;
 
 		$checked = $db->get_categories_checked_array();
 
@@ -15,8 +16,5 @@
 			'text' => "دسته بندی هایی که می‌خواهید مطالبشان برایتان ارسال شود را انتخاب کنید.\n پست های جدید مربوط به دسته های انتخاب شده از طریق ربات برای شما ارسال می‌گردد.",
 			'reply_markup' => $reply_markup,
 		]);
-
-
-	}	
-
-?>
+	}
+}
